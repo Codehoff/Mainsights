8544bf8eed3aab39bc10ea5b6b34bf3431fe6afb
Codehoff <enno.kirchhoff1911@gmail.com>
2020-08-19 16:02:20 +0200

    changed questions

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -3,23 +3,22 @@ import "./models/flashcard.dart";
 var dummyFlashcards = [
   Flashcard(
     id: 'q1',
-    question:
-        'How can Chewbacca live on Endor with the Ewoks, when he is clearly a Wookie that should be on Kashyyk?',
-    answer: "Oh well that is a good question, I don't know",
+    question: 'What is your name?',
+    answer: "'tis Arthur, King of the Britains'",
     category: Category.Process,
     complexity: Complexity.Advanced,
   ),
   Flashcard(
     id: 'q2',
-    question: 'What is your name?',
-    answer: "'tis Arthur, King of the Britains",
+    question: 'What is your quest?',
+    answer: "To seek the holy grail!",
     category: Category.Process,
     complexity: Complexity.Indermediate,
   ),
   Flashcard(
     id: 'q3',
-    question: 'What is your quest?',
-    answer: "To seek the holy grail!",
+    question: 'What is the airspeed velocity of an unladen swallow?',
+    answer: "What do you mean? African or american swallow?",
     category: Category.Valuation,
     complexity: Complexity.Basic,
   ),
