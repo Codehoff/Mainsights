ad2d142d2656cd5c5c790523e3a5432a7900030a
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-09-22 18:14:10 +0200

    Cleanup 2

--- lib/screens/studyscreen.dart
+++ lib/screens/studyscreen.dart
@@ -1,10 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:flutter_complete_guide/widgets/flashcards_show.dart';
-import 'package:swipedetector/swipedetector.dart';
-import 'package:provider/provider.dart';
-
-import "../models/flashcard.dart";
-import '../providers/flashcards.dart';
 
 class Studyscreen extends StatelessWidget {
   static const routeName = "/studyscreen";
