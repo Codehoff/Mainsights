9878b3f3a6ee2f818ab1348114106468d237c804
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-09-30 15:10:46 +0200

    Update study_selection_screen.dart

--- lib/screens/study_selection_screen.dart
+++ lib/screens/study_selection_screen.dart
@@ -51,141 +51,145 @@ class _StudySelectionScreenState extends State<StudySelectionScreen> {
         child: MainDrawer(),
       ),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 10),
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColorLight,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Enter your search terms below",
-                      style: TextStyle(fontSize: 20),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "In case you want to specify your search results, choose the respective chapter",
-                      style: TextStyle(fontSize: 14),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColorLight,
+                borderRadius: BorderRadius.circular(10),
               ),
-              SizedBox(
-                height: 50,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Enter your search terms below",
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "In case you want to specify your search results, choose the respective chapter",
+                    style: TextStyle(fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
               ),
-              Text(
-                "Category",
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              DropdownButton<String>(
-                isExpanded: true,
-                items: _categories.map((String dropdownValue1) {
-                  return DropdownMenuItem<String>(
-                    value: dropdownValue1,
-                    child: Text(dropdownValue1),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  _onSelectedCategory(value);
-                  dropdownValue1 = value;
-                },
-                value: _selectedCategories,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Subcategory",
-                style: TextStyle(fontSize: 16),
-              ),
-              DropdownButton<String>(
-                isExpanded: true,
-                items: _subcategories.map((String dropdownValue2) {
-                  return DropdownMenuItem<String>(
-                    value: dropdownValue2,
-                    child: Text(dropdownValue2),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  _onSelectedSubcategory(value);
-                  dropdownValue2 = value;
-                },
-                value: _selectedSubcategory,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Difficulty",
-                style: TextStyle(fontSize: 16),
-              ),
-              DropdownButton<String>(
-                isExpanded: true,
-                items: <String>["All", "Basic", "Intermediate", "Advanced"]
-                    .map((String dropdownValue3) {
-                  return DropdownMenuItem<String>(
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Category",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    items: _categories.map((String dropdownValue1) {
+                      return DropdownMenuItem<String>(
+                        value: dropdownValue1,
+                        child: Text(dropdownValue1),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      _onSelectedCategory(value);
+                      dropdownValue1 = value;
+                    },
+                    value: _selectedCategories,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Subcategory",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    items: _subcategories.map((String dropdownValue2) {
+                      return DropdownMenuItem<String>(
+                        value: dropdownValue2,
+                        child: Text(dropdownValue2),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      _onSelectedSubcategory(value);
+                      dropdownValue2 = value;
+                    },
+                    value: _selectedSubcategory,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Difficulty",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    items: <String>["All", "Basic", "Intermediate", "Advanced"]
+                        .map((String dropdownValue3) {
+                      return DropdownMenuItem<String>(
+                        value: dropdownValue3,
+                        child: Text(dropdownValue3),
+                      );
+                    }).toList(),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue3 = newValue;
+                      });
+                    },
                     value: dropdownValue3,
-                    child: Text(dropdownValue3),
-                  );
-                }).toList(),
-                onChanged: (String newValue) {
-                  setState(() {
-                    dropdownValue3 = newValue;
-                  });
-                },
-                value: dropdownValue3,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Viewed / Not viewed",
-                style: TextStyle(fontSize: 16),
-              ),
-              DropdownButton<String>(
-                isExpanded: true,
-                items: <String>["All", "Not viewed", "Viewed"]
-                    .map((String dropdownValue4) {
-                  return DropdownMenuItem<String>(
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Viewed / Not viewed",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    items: <String>["All", "Not viewed", "Viewed"]
+                        .map((String dropdownValue4) {
+                      return DropdownMenuItem<String>(
+                        value: dropdownValue4,
+                        child: Text(dropdownValue4),
+                      );
+                    }).toList(),
+                    onChanged: (String newerValue) {
+                      setState(() {
+                        dropdownValue4 = newerValue;
+                      });
+                    },
                     value: dropdownValue4,
-                    child: Text(dropdownValue4),
-                  );
-                }).toList(),
-                onChanged: (String newerValue) {
-                  setState(() {
-                    dropdownValue4 = newerValue;
-                  });
-                },
-                value: dropdownValue4,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () => changePage(dropdownValue1, dropdownValue2),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    splashColor: Colors.white,
+                    child: Text(
+                      "Go",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 80,
-              ),
-              FloatingActionButton(
-                onPressed: () => changePage(dropdownValue1, dropdownValue2),
-                backgroundColor: Theme.of(context).primaryColor,
-                splashColor: Colors.white,
-                child: Text(
-                  "Go",
-                  style: TextStyle(fontSize: 16.0),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
